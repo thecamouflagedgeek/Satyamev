97d9d7b9629e51d00a76d17557d3001c00795e0e
thecamouflagedgeek <pretentiousgazette@gmail.com>
2026-02-25 22:05:15 +0530

    Pycache removed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware 
 from app.api.routes import router 
-
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost:3000",
+    "https://your-frontend.vercel.app"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app=FastAPI(title="Satyamev")
-
-app.add_middleware(CORSMiddleware,
-                   allow_origins=["*"],
-                   allow_methods=["*"],
-                   allow_credentials=True,
-                   allow_headers=["*"])
 
 app.include_router(router)
 @app.get("/")
