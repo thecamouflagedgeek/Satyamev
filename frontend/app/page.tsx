355665acc685c0dfbc7c7f14062bd168ad49ce3c
thecamouflagedgeek <pretentiousgazette@gmail.com>
2026-03-25 14:40:27 +0530

    Backend url added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   useEffect(() => {
     const fetchIssues = async () => {
       try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/issues`);
+        const res = await fetch("https://satyamev.onrender.com/issues");
         const data = await res.json();
         setIssues(data);
       } catch (err) {
